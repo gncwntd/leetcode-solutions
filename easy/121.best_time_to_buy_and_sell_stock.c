22bc1a3b0a8e62fac1e9d98d8ca88b5e42097fc8
mrwhocares <emregenc12345@gmail.com>
2026-08-20 16:59:12 +0300

    121 | solved by tracking the minimum price and calculating the maximum profit in one pass using a greedy approach | 0 ms (100.00%), 15.97 MB (86.98%)

--- easy/121.best_time_to_buy_and_sell_stock.c
+++ easy/121.best_time_to_buy_and_sell_stock.c
@@ -1,18 +1,15 @@
 int maxProfit(int* prices, int pricesSize) {
+    int maxprofit = 0;
     int min = prices[0];
-    int max = 0;
 
-    for (int i = 1; i < pricesSize; i++) {
-        if (prices[i] < min) {
+    for(int i = 1; i < pricesSize;i++){
+        if(prices[i] < min){
             min = prices[i];
         }
-
         int profit = prices[i] - min;
-
-        if (profit > max) {
-            max = profit;
-        }
+        
+        if(profit > maxprofit) maxprofit = profit;
     }
 
-    return max;
+    return maxprofit;
 }
