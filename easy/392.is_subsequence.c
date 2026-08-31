1a7fce7482e235e43d5d82e34add071cdac91182
mrwhocares <emregenc12345@gmail.com>
2026-08-31 23:27:24 +0300

    392 | solved using two pointers to check if s is a subsequence of t | 0 ms (100.00%), 8.77 MB (4.75%)

--- easy/392.is_subsequence.c
+++ easy/392.is_subsequence.c
@@ -1,20 +1,20 @@
-
-bool canConstruct(char* ransomNote, char* magazine) {
-    int count[26] = {0};
-
-    for (int i = 0; magazine[i] != '\0'; i++) {
-        count[magazine[i] - 'a']++;
+bool isSubsequence(char* s, char* t) {
+    
+    int i = 0;
+    for(int j = 0; t[j] != '\0';j++){
+        if(s[i] == t[j]) i++;
     }
-
-    for (int i = 0; ransomNote[i] != '\0'; i++) {
-        int index = ransomNote[i] - 'a';
-
-        if (count[index] == 0) {
-            return false;
-        }
-
-        count[index]--;
-    }
-
-    return true;
+    return s[i] == '\0';
 }
+/*
+/*
+Runtime
+0
+ms
+Beats
+100.00%
+Memory
+8.77
+MB
+Beats
+4.75%*/*/
